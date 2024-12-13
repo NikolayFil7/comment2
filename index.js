27471a0867712c75e8e39193b3e475dfdbef15ba
NikolayFil7 <krasavcheknik@protonmail.com>
2024-12-14 00:49:38 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+//JavaScript-код с созданием переменных
+
+/*Автор
+Николай*/
+
+let myName = "Nikolai"
+console.log(myName)
+const mySurname = "Filippov"
+console.log(mySurname)
+let myFevoriteDrink = "Tea"
+console.log(myFevoriteDrink)
+let myFavoriteAnimal = "Cat"
+console.log(myFavoriteAnimal)
+let myFavoriteProgrammingLainguage = "Python"
+console.log(myFavoriteProgrammingLainguage)
